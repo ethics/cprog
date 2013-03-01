dbd01e9a0f80defa1979d7f40827f711db1a7a49
siva <sivakumar.bs@gmail.com>
2013-03-01 18:35:33 +0530

    Changes in printf and Makefile

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ CC=gcc
 
 hello:hello.o
 	$(CC) -o hello hello.o
+
+static:static.o
+	$(CC) -o static static.o
+
--- static.c
+++ static.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+
 void main()
 {
  static int i;
- printf("The value of i is %d,i);
+ printf("The value of i is %d",i);
 }
